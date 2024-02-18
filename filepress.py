c6395963ac41fefcb27582d0b8aef5fbbda28fec
Ayush Teke <tekeayush@proton.me>
2024-02-18 22:16:19 +0530

    adding exception handlers and some methods

--- filepress.py
+++ filepress.py
@@ -1,32 +1,54 @@
-from cfscrape import create_scraper
 from urllib.parse import urlparse
+import requests
 
 url = input('url: ')
 
-def filepress(url):
-    cget = create_scraper().request
+def filepress(url, api_url, api2_url):
+    client = requests.session()
     try:
-        url = cget('GET', url).url
+        res = client.get(url)
+        res.raise_for_status()
         raw = urlparse(url)
         json_data = {
             'id': raw.path.split('/')[-1],
             'method': 'cloudR2Downlaod',
-            }
-        api = f'{raw.scheme}://{raw.hostname}/api/file/downlaod/'
-        api2 = f'https://new8.filepress.store/api/file/downlaod2/'
-        res = cget('POST', api, headers={'Referer': f'{raw.scheme}://{raw.hostname}'}, json=json_data).json()
-    except Exception as e:
-        return(f'ERROR: {e.__class__.__name__}')
-    if 'data' not in res:
-        return(f'ERROR: {res["statusText"]}')
+        }
+        api = api_url
+        api2 = api2_url
+
+        res = client.post(api,
+                          headers={
+                              'Referer': f'{raw.scheme}://{raw.hostname}'
+                          },
+                          json=json_data)
+        res.raise_for_status()  
+    except requests.exceptions.RequestException as e:
+        return f'ERROR: {e}'
+
+    if 'data' not in res.json():
+        return f'ERROR: {res.json()["statusText"]}'
     else:
         json_data = {
-            'id': res['data']['downloadId'],
+            'id': res.json()['data']['downloadId'],
             'method': 'cloudR2Downlaod',
-            }
-        res = cget('POST', api2, headers={'Referer': f'{raw.scheme}://{raw.hostname}'}, json=json_data)
+        }
+        res = client.post(api2,
+                          headers={'Referer': f'{raw.scheme}://{raw.hostname}'},
+                          json=json_data)
+        res.raise_for_status()  
         if res.status_code == 200:
             return res.json()['data']
         else:
             return f'Failed At API2 With Status {res.json()["statusText"]}'
-print(filepress(url))
+
+
+api_url = 'https://new9.filepress.store/api/file/downlaod/'
+api2_url = 'https://new9.filepress.store/api/file/downlaod2/'
+
+print(filepress(url, api_url, api2_url))
+
+
+# alternate methods of downloads
+#indexDownlaod
+#cloudDownlaod
+#cloudR2Downlaod (Best)
